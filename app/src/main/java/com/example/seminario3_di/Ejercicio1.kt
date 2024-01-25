2eb38c7e9f1ad94ee88bbda75493282754b702d2
bakaruu <arucastro@gmail.com>
2024-01-25 20:04:02 +0100

    Ejercicio 1 countdown hecho

---
+++ app/src/main/java/com/example/seminario3_di/Ejercicio1.kt
@@ -0,0 +1,60 @@
+package com.example.seminario3_di
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.seminario3_di.databinding.ActivityEjercicio1Binding
+
+class Ejercicio1: AppCompatActivity() {
+
+    private lateinit var tvEjercicio1: TextView
+    private lateinit var countDownTimer: CountDownTimer
+
+    private lateinit var binding: ActivityEjercicio1Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEjercicio1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        tvEjercicio1 = binding.tvEjercicio1
+
+
+        val tiempoTotal: Long = 10000
+
+        // Inicia el temporizador
+        iniciarTemporizador(tiempoTotal)
+
+
+    }
+
+    private fun iniciarTemporizador(tiempoTotal: Long) {
+        countDownTimer = object : CountDownTimer(tiempoTotal, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                // Se llama cada segundo mientras el temporizador está en marcha
+                val segundosRestantes = millisUntilFinished / 1000
+                tvEjercicio1.text = "Tiempo restante: $segundosRestantes segundos"
+            }
+
+            override fun onFinish() {
+                // Se llama cuando el temporizador llega a cero
+                tvEjercicio1.text = "¡Tiempo agotado!"
+            }
+        }
+
+        // Inicia el temporizador
+        countDownTimer.start()
+    }
+
+    override fun onDestroy() {
+        // Asegúrate de detener el temporizador cuando la actividad se destruye
+        countDownTimer.cancel()
+        super.onDestroy()
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/seminario3_di/MainActivity.kt
+++ app/src/main/java/com/example/seminario3_di/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.seminario3_di
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.seminario3_di.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btmEjercicio1: Button
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        btmEjercicio1 = binding.btnEjercicio1
+        binding.btnEjercicio1.setOnClickListener {
+            val intent = Intent(this, Ejercicio1::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
