8991a569cb270fad54e305bee4642c325bb69e63
Alvaro Castro <arucastro@gmail.com>
2024-02-01 14:16:13 +0100

    update

--- app/src/main/java/com/example/seminario3_di/Ejercicio1.kt
+++ app/src/main/java/com/example/seminario3_di/Ejercicio1.kt
@@ -1,34 +1,29 @@
 package com.example.seminario3_di
-
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.os.Handler
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.seminario3_di.databinding.ActivityEjercicio1Binding
 
-class Ejercicio1: AppCompatActivity() {
+class Ejercicio1 : AppCompatActivity() {
 
     private lateinit var tvEjercicio1: TextView
     private lateinit var countDownTimer: CountDownTimer
-
     private lateinit var binding: ActivityEjercicio1Binding
+    private var tiempoTotal: Long = 5000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEjercicio1Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         tvEjercicio1 = binding.tvEjercicio1
 
-
-        val tiempoTotal: Long = 3000
-
-        // Inicia el temporizador
-        iniciarTemporizador(tiempoTotal)
-
-
+        // Inicia el temporizador solo si savedInstanceState es nulo, es decir, si es la primera vez que se crea la actividad
+        if (savedInstanceState == null) {
+            iniciarTemporizador(tiempoTotal)
+        }
     }
 
     private fun iniciarTemporizador(tiempoTotal: Long) {
@@ -41,17 +36,16 @@ class Ejercicio1: AppCompatActivity() {
 
             override fun onFinish() {
                 // Se llama cuando el temporizador llega a cero
-
                 val intent = Intent(this@Ejercicio1, Ejercicio1Boom::class.java)
                 startActivity(intent)
-
             }
-
-        // Inicia el temporizador
         }.start()
+    }
 
-
-
+    override fun onResume() {
+        super.onResume()
+        // Reinicia el temporizador cuando la actividad se reanuda
+        iniciarTemporizador(tiempoTotal)
     }
 
     override fun onDestroy() {
@@ -65,9 +59,4 @@ class Ejercicio1: AppCompatActivity() {
         // Asegúrate de detener el temporizador cuando la actividad se pausa
         countDownTimer.cancel()
     }
-
-
-
-
-
 }
